8d2fd52948f1aba751ad01d8766e41a2c6125bdd
Barma Vishwanth <vishwanthbsavp@gmail.com>
2022-08-15 21:55:00 +0530

    Implemented: connectionHandler

--- src/Server.java
+++ src/Server.java
@@ -1,22 +1,53 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server implements Runnable{
+
+    private ArrayList<ConnectionHandler> connections;
 
     @Override
     public void run() {
         try {
             ServerSocket server = new ServerSocket(9999);
+            Socket client = server.accept();
+            ConnectionHandler handler = new ConnectionHandler(client);
+            connections.add(handler);
         } catch (IOException e) {
-            e.printStackTrace();
+            //TODO: shutdown function
         }
     }
 
     class ConnectionHandler implements Runnable{
 
+        private Socket client;
+        private BufferedReader in;
+        private PrintWriter out;
+        private String name;
+
+        public ConnectionHandler(Socket client){
+            this.client = client;
+        }
+
         @Override
         public void run() {
+            try {
+                out = new PrintWriter(client.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                out.println("Enter Your Name: ");
+                name = in.readLine();
+                System.out.println(name + " connected!");
+            } catch (IOException e) {
+                //TODO: shutdown function
+            }
+        }
 
+        public void sendMessage(String message){
+            out.println(message);
         }
     }
 }
